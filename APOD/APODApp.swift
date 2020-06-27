7ee8771e85b8bdb031fcf2e98b3d57f07d9a6e55
Izumu Mishima <31067139+izmishi@users.noreply.github.com>
2020-06-28 00:14:07 +0100

    Initial Commit

---
+++ APOD/APODApp.swift
@@ -0,0 +1,17 @@
+//
+//  APODApp.swift
+//  APOD
+//
+//  Created by Izumu Mishima on 28/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct APODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
